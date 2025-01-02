5744aacafc20e10238d2bd98781424afe55d170b
Julian Ding <scorpiusa51@gmail.com>
2025-01-02 11:52:04 -0800

    Make gamma scaling part of tone mapping

--- brightness/dino.py
+++ brightness/dino.py
@@ -23,25 +23,12 @@ def resize_image(
             resize_height = resize_width / width * height
         resize_width = round(resize_width)
         resize_height = round(resize_height)
-        if height != resize_height or width != resize_width:
+        # Only resize down
+        if height > resize_height or width > resize_width:
             image = cv2.resize(
                 image, (resize_width, resize_height), interpolation=cv2.INTER_CUBIC
             )
     return image
-
-
-def read_greyscale_hdr(file_path: str, gamma: float = 1.0):
-    """Load greyscale HDR image from disk as 2D numpy array of luminances.
-
-    If loading brightness response, leave gamma = 1.
-    If loading greyscale scene, set gamma = 2.2.
-    """
-    if not any(file_path.endswith(ext) for ext in [".exr", ".hdr"]):
-        raise ValueError("Unsupported file format.")
-
-    image = cv2.imread(file_path, cv2.IMREAD_ANYDEPTH).astype(np.float32)
-    image[image < 0] = 0  # Negative values shouldn't exist
-    return np.power(image, 1.0 / gamma)
 
 
 def read_image(
@@ -52,37 +39,46 @@ def read_image(
     Assumes EXR images are in units of nits (candela/m^2).
     Assumes SDR images are displayed on a monitor at the specified luminance (nits) for pure white.
 
-    Returns image with RGB channels in nits.
+    Returns image with RGB channels in nits (linear luminances).
     """
-    if any(file_path.endswith(ext) for ext in [".hdr"]):
-        image = cv2.imread(file_path, cv2.IMREAD_ANYDEPTH).astype(np.float32)
-    elif any(file_path.endswith(ext) for ext in [".png", ".jpg", ".jpeg", ".exr"]):
-        image = cv2.imread(file_path, cv2.IMREAD_UNCHANGED).astype(np.float32)
-    else:
-        raise ValueError("Unsupported file format.")
+    bgr_image = cv2.imread(file_path, cv2.IMREAD_UNCHANGED).astype(np.float32)
 
     # RGB or RGBA only
-    assert len(image.shape) == 3
-    assert image.shape[2] >= 3
+    assert len(bgr_image.shape) == 3
+    assert bgr_image.shape[2] >= 3
     # Negative values shouldn't exist
-    image[image < 0] = 0
+    bgr_image[bgr_image < 0] = 0
     # OpenCV uses BGR
-    b = image[:, :, 0]
+    b = bgr_image[:, :, 0]
+    g = bgr_image[:, :, 1]
+    r = bgr_image[:, :, 2]
+    image = np.stack([r, g, b], axis=-1)
+
+    if not any(file_path.endswith(ext) for ext in [".hdr", ".exr"]):
+        # uint8 for SDR images (0, 255)
+        image = (image / 255) ** gamma
+        image *= white_nits
+
+    return image
+
+
+def write_image(file_path: str, image: np.ndarray, gamma: float = 2.2):
+    """Write an SDR RGB image array to file_path.
+
+    image must have dimensions (w, h, 3) and take values from (0, 1).
+    Values less than 0 will be clipped to 0.
+    Values greater than 1 will be clipped to 1.
+    """
+    image[image < 0] = 0
+    image[image > 1] = 1
+    # Use nonlinear luminance for SDR
+    image = image ** (1.0 / gamma)
+    image = (image * 255).astype(np.int32)
+    # RGB to BGR
+    r = image[:, :, 0]
     g = image[:, :, 1]
-    r = image[:, :, 2]
-
-    if any(file_path.endswith(ext) for ext in [".hdr", ".exr"]):
-        # Apply gamma correction
-        r = np.power(r, 1.0 / gamma)
-        g = np.power(g, 1.0 / gamma)
-        b = np.power(b, 1.0 / gamma)
-    else:
-        # OpenCV uses BGR and uint8 for SDR images (0, 255)
-        r = r * white_nits / 255
-        g = g * white_nits / 255
-        b = b * white_nits / 255
-
-    return np.stack([r, g, b], axis=-1)
+    b = image[:, :, 2]
+    cv2.imwrite(file_path, np.stack([b, g, r], axis=-1))
 
 
 def rgb_to_xyz(image: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -94,7 +90,7 @@ def rgb_to_xyz(image: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
             [0.0193339, 0.1191920, 0.9503041],
         ]
     )
-    xyz = np.dot(image, rgb_to_xyz_matrix.T)
+    xyz = image @ rgb_to_xyz_matrix.T
     return xyz[..., 0], xyz[..., 1], xyz[..., 2]  # X, Y, Z
 
 
@@ -135,9 +131,11 @@ def lxy_to_rgb(
     return rgb
 
 
-def gaussian_blur_cv(image: np.ndarray, sigma: float) -> np.ndarray:
+def gaussian_blur_cv(
+    image: np.ndarray, sigma: float, kernel_sigmas: int = 1
+) -> np.ndarray:
     # Ensure the kernel size is odd
-    kernel_size = int(6 * sigma + 1) | 1
+    kernel_size = int(2 * kernel_sigmas * sigma + 1) | 1
     blurred = cv2.GaussianBlur(
         image,
         (kernel_size, kernel_size),
@@ -163,6 +161,7 @@ def generate_scales(
 
 def dn_brightness_model(
     L: np.ndarray,
+    gamma: float = 2.2,
     cs_ratio: float = 2.0,
     min_scale: float = 1.0,
     w: float = 0.85,
@@ -172,7 +171,7 @@ def dn_brightness_model(
     d: float = 1.0,
     scale_normalized_constants: bool = False,
 ) -> np.ndarray:
-    """Apply divisive normalization brightness model to array of luminances (i.e. greyscale input image).
+    """Apply divisive normalization brightness model to array of linear luminances.
 
     B(x,y) = sum(
         w**i * (
@@ -184,11 +183,13 @@ def dn_brightness_model(
     NOTE: The current scale is the center stdev at the current scale,
           the next scale up is the surround stdev at the current scale.
     """
+    L = L * (1.0 / gamma)
+
     scales = generate_scales(L.shape, cs_ratio, min_scale)
     weights = [w**i for i in range(len(scales))]
 
     # Initialize weighted_sum as a 2D array
-    weighted_sum = np.zeros(L.shape[:2], dtype=L.dtype)
+    weighted_sum = np.zeros(L.shape[:2])
 
     # Compute ratios and weighted sum using only two blurred images at a time
     center_response = gaussian_blur_cv(L, scales[0])
