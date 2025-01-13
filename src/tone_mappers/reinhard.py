51265bdb0216f62a8f01f2958ff934c93a81b243
Julian Ding <39065162+ASavageCabbage@users.noreply.github.com>
2025-01-13 12:56:54 -0800

    Fix Reinhard tone mappers

--- src/tone_mappers/reinhard.py
+++ src/tone_mappers/reinhard.py
@@ -2,17 +2,8 @@ import numpy as np
 
 
 def reinhard(
-    image: np.ndarray, key: float = 0.18, L_white: float = 1.0
+    image: np.ndarray, key: float = 0.18, l_white: float = 1.0, gamma: float = 2.2
 ) -> np.ndarray:
     """Tone mapper described by Reinhard et al"""
-    gamma = 2.2
-    # Reinhard takes the log average and this is akin to taking the average of gamma corrected values and linearizing it
-    average = np.mean(image)
-    linearized_average = np.power(average, gamma)
-    linearized_luminances = np.power(image, gamma)
-    scaled_linear_luminances = (key / linearized_average) * linearized_luminances
-    tone_mapped = scaled_linear_luminances / (
-        1 + scaled_linear_luminances / (L_white**2)
-    )
-    # gamma_corrected_tone_mapped = np.power(tone_mapped, 1.0/gamma)
-    return tone_mapped
+    image = (key / (np.mean(image ** (1.0/gamma)) ** gamma)) * image
+    return image / (1 + image / (l_white**2))
